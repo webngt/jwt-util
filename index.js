3b03018a89fa6f09df77b6b213c46d794c30106e
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-15 00:10:14 +0300

    generate enc token

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 
 (async () => {
     const keystore = jose.JWK.createKeyStore();
-    await keystore.generate('RSA', 2048, {alg: 'RS256', use: 'sig' });
+
+    // enc key
+    await keystore.generate('RSA', 2048, {use: 'enc'});
     fs.writeFileSync(
         'secret.json', 
         JSON.stringify(keystore.toJSON(true), null, '  ')
     );
-    fs.writeFileSync(
-        'public.json', 
-        JSON.stringify(keystore.toJSON(false), null, '  ')
-    );
+
+
 })();
